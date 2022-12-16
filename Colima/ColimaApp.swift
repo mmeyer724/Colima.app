39286113b9671bfcaac4c4b20beaa2a0127510c4
Mike Meyer <michael@meyer.io>
2022-12-16 16:30:41 -0500

    Initial Commit

---
+++ Colima/ColimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColimaApp.swift
+//  Colima
+//
+//  Created by Mike Meyer on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ColimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
